f25e71e5c2f1662937ae3627f01c412412b6cfd5
Tamas Nagy <tamas@tamasnagy.com>
2016-04-26 15:07:13 -0700

    add PSF to each phenotype during sorting and bin
    The original model was a bit too clean: almost all measurements of
    phenotype off the FACS machine look normal or log-normal. Here we're
    convoluting the theoretical phenotype with a normal distribution and then
    binning the cells.

--- src/selection.jl
+++ src/selection.jl
@@ -1,14 +1,27 @@
-binprob(p::Float64) = (.3*p+.5, .5-.3*p)
-
 """
 Given `cells`, a vector of integers, and `guides`, a vector of barcodes
-performs simulated facs sorting of the cells.
+performs simulated facs sorting of the cells into `bins` with the given
+cutoffs. `σ` refers to the spread of the phenotype during the FACS screen.
 """
-function facs_sort_hinting(cells::Vector{Int64}, guides::Vector{Barcode})
-    bins = Array{Int64}[Int64[], Int64[]]
-    for cell in cells
-        choices = binprob(guides[cell].theo_phenotype, prob_prealloc)
-        push!(bins[rand(Categorical([choices[1], choices[2]]))], cell)
+function facs_sort(cells::Vector{Int64}, guides::Vector{Barcode},
+                   bins::Dict{Symbol, Tuple{Float64, Float64}}, σ::Float64)
+
+    binned_cells = Array{Int64}[Int64[] for _ in length(keys(bins))]
+    n_cells = length(cells)
+    observed = zeros(n_cells)
+    indices = collect(1:n_cells)
+    for i in 1:n_cells
+        cell = cells[i]
+        observed[i] = rand(Normal(guides[cell].theo_phenotype, σ))
+        guides[cell].obs_phenotype = observed[i]
     end
-    bins
+    sortperm!(indices, observed)
+    permute!(cells, indices)
+    results = Dict{Symbol, Vector{Int64}}()
+    for (binname, cutoffs) in bins
+        left = clamp(round(Int64, cutoffs[1]*n_cells), 1, n_cells)
+        right = clamp(round(Int64, cutoffs[2]*n_cells), 1, n_cells)
+        results[binname] = cells[left:right]
+    end
+    results
 end
