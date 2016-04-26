9444ac5eaa205e5edb5bac324c44bf9bbda110f7
Tamas Nagy <tamas@tamasnagy.com>
2016-04-26 15:22:16 -0700

    remove unnecessary preallocation code

--- src/selection.jl
+++ src/selection.jl
@@ -6,18 +6,17 @@ cutoffs. `σ` refers to the spread of the phenotype during the FACS screen.
 function facs_sort(cells::Vector{Int64}, guides::Vector{Barcode},
                    bins::Dict{Symbol, Tuple{Float64, Float64}}, σ::Float64)
 
-    binned_cells = Array{Int64}[Int64[] for _ in length(keys(bins))]
     n_cells = length(cells)
     observed = zeros(n_cells)
-    indices = collect(1:n_cells)
     for i in 1:n_cells
         cell = cells[i]
         observed[i] = rand(Normal(guides[cell].theo_phenotype, σ))
         guides[cell].obs_phenotype = observed[i]
     end
-    sortperm!(indices, observed)
-    permute!(cells, indices)
+    indices = sortperm(observed)
+    cells = cells[indices]
     results = Dict{Symbol, Vector{Int64}}()
+
     for (binname, cutoffs) in bins
         left = clamp(round(Int64, cutoffs[1]*n_cells), 1, n_cells)
         right = clamp(round(Int64, cutoffs[2]*n_cells), 1, n_cells)
