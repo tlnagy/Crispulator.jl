aed77e38c3d1fa5caec5618e764e01c452815f2a
Tamas Nagy <tamas@tamasnagy.com>
2016-04-27 18:16:48 -0700

    compute auroc and add roc plotting code

--- src/analysis.jl
+++ src/analysis.jl
@@ -13,17 +13,26 @@ function analyze(raw_data::Vector{DataFrame}; gen_plots=false)
     nonnegs = combined[combined[:class] .!= :negcontrol, :]
     negcontrols = combined[combined[:class] .== :negcontrol, :log2fc]
 
+    genes = by(nonnegs, [:gene, :behavior, :class]) do barcodes
+        result = MannWhitneyUTest(barcodes[:log2fc], negcontrols)
+        DataFrame(pvalue = -log10(pvalue(result)), mean= mean(barcodes[:log2fc]))
+    end
+    genes[:absmean] = abs(genes[:mean])
+    genes[:pvalmeanprod] = genes[:absmean] .* genes[:pvalue]
+
+    auroc, tprs, fprs = compute_roc(genes, :pvalmeanprod, 50)
+
     if gen_plots
-        df = by(nonnegs, [:gene, :behavior, :class]) do barcodes
-            result = MannWhitneyUTest(barcodes[:log2fc], negcontrols)
-            DataFrame(pvalue = -log10(pvalue(result)), mean= mean(barcodes[:log2fc]))
-        end
         draw(PDF("plots/volcano_plot_by_behavior.pdf", 12cm, 10cm),
-        plot(df, x=:mean, y=:pvalue, color=:behavior, Guide.xlabel("mean log2 FC"),
+        plot(genes, x=:mean, y=:pvalue, color=:behavior, Guide.xlabel("mean log2 FC"),
         Guide.ylabel("-log10 pvalue"), Theme(highlight_width=0pt)))
         draw(PDF("plots/volcano_plot_by_class.pdf", 12cm, 10cm),
-        plot(df, x=:mean, y=:pvalue, color=:class, Theme(highlight_width=0pt)))
+        plot(genes, x=:mean, y=:pvalue, color=:class, Theme(highlight_width=0pt)))
         draw(PDF("plots/distributions_of_observed_phenotypes.pdf", 12cm, 6cm),
         plot(combined, x=:obs_phenotype, color=:class, Geom.density))
+        draw(PDF("plots/roc.pdf", 12cm, 10cm),
+        plot(x=fprs, y=tprs, Geom.line, Coord.cartesian(fixed=true),
+        Guide.xlabel("fpr"), Guide.ylabel("tpr")))
     end
+    auroc
 end
