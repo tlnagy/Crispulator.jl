b5a2e04b39b395be1d3920e217841126d3d4b7ee
Tamas Nagy <tamas@tamasnagy.com>
2016-04-28 15:56:24 -0700

    move run code into a function for easier profiling
    Working on #6

--- src/run.jl
+++ src/run.jl
@@ -3,8 +3,8 @@ using Distributions
 using Gadfly
 using HypothesisTests
 
-include("utils.jl")
 include("common.jl")
+include("utils.jl")
 include("library.jl")
 include("transfection.jl")
 include("selection.jl")
@@ -14,19 +14,26 @@ include("analysis.jl")
 const N = 500 # number of target genes
 const coverage = 5 # number of guides per gene
 const representation = 1000 # Number of cells with each guide
-
-guides, guide_freqs = construct_library(N, coverage)
-
-cell_count = N*coverage*representation
 const moi = 0.25
-guide_freqs_dist = Categorical(guide_freqs)
 
-cells = transfect(guide_freqs_dist, cell_count, moi)
+function run_exp()
 
-bin_info = Dict(:bin1 => (0.0, 1/3), :bin2 => (2/3, 1.0))
-bin_cells = facs_sort(cells, guides, bin_info, 0.5)
+    guides, guide_freqs = construct_library(N, coverage)
 
-freqs = counts_to_freqs([bin_cells[binname] for binname in keys(bin_cells)]...)
-raw_data = sequencing([10^7, 10^7], guides, freqs...)
+    cell_count = N*coverage*representation
+    guide_freqs_dist = Categorical(guide_freqs)
 
-analyze(raw_data, gen_plots=true)
+    cells = transfect(guide_freqs_dist, cell_count, moi)
+
+    bin_info = Dict(:bin1 => (0.0, 1/3), :bin2 => (2/3, 1.0))
+    bin_cells = facs_sort(cells, guides, bin_info, 0.5)
+
+    freqs = counts_to_freqs([bin_cells[binname] for binname in keys(bin_cells)]...)
+    raw_data = sequencing([10^7, 10^7], guides, freqs...)
+
+    auroc = analyze(raw_data, gen_plots=false)
+
+    auroc
+end
+
+@time run_exp()
