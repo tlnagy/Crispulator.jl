4d72dab24f1421e95e2d7661dc5482939e0e4b17
Tamas Nagy <tamas@tamasnagy.com>
2016-05-02 16:39:47 -0700

    add linear expansion after transfection, working on #2

--- src/transfection.jl
+++ src/transfection.jl
@@ -2,6 +2,10 @@ function transfect(guides::Vector{Barcode}, guide_freqs_dist::Distributions.Cate
                    cell_count::Int64, moi::Float64)
     cells = rand(guide_freqs_dist, round(Int64, pdf(Poisson(moi), 1)*cell_count))
 
+    # simple linear expansion of 10-fold
+    # Maybe play around with alternative strategies here
+    cells = repeat(cells, inner=[10])
+
     initial_freqs = StatsBase.counts(cells, 1:length(guides)) ./ length(cells)
 
     for i in 1:length(guides)
