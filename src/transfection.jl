3bdf0b4bf6127776c93936615ce08eb700635e8d
Tamas Nagy <tamas@tamasnagy.com>
2016-05-04 12:31:35 -0700

    fix performance regression, working on #6

--- src/transfection.jl
+++ src/transfection.jl
@@ -1,10 +1,15 @@
 function transfect(guides::Vector{Barcode}, guide_freqs_dist::Distributions.Categorical,
                    cell_count::Int64, moi::Float64)
     cells = rand(guide_freqs_dist, round(Int64, pdf(Poisson(moi), 1)*cell_count))
+    num_cells = length(cells)
 
     # simple linear expansion of 10-fold
     # Maybe play around with alternative strategies here
-    cells = repeat(cells, inner=[10])
+    expansion = Array(Int64, num_cells*10)
+    for rep in 1:10
+        @inbounds expansion[(rep-1)*num_cells+1:rep*num_cells] = cells
+    end
+    cells = expansion
 
     initial_freqs = StatsBase.counts(cells, 1:length(guides)) ./ length(cells)
 
