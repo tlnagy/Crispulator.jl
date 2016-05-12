8ee6fff0612f315c3519d8b0fdfd5921137ef1a9
Tamas Nagy <tamas@tamasnagy.com>
2016-05-12 16:52:59 -0700

    normalize to median freq of negcontrols, fixes #19

--- src/processing.jl
+++ src/processing.jl
@@ -5,14 +5,22 @@ function difference_between_two_bins(raw_data::Dict{Symbol, DataFrame})
         # taking the log
         seq_data[:counts] += 0.5
         seq_data[:freqs] = seq_data[:counts]./sum(seq_data[:counts])
+        # normalize to median of negative controls, fixes #19
+        # TODO: consider normalizing the std dev
+        med = median(seq_data[seq_data[:class] .== :negcontrol, :freqs])
+        seq_data[:rel_freqs] = seq_data[:freqs] ./ med
     end
     @assert length(keys(raw_data)) == 2 "exactly two bins needed"
 
     combined = copy(raw_data[:bin1])
-    rename!(combined, Dict(:freqs => :freqs1, :counts => :counts1))
+    rename!(combined, Dict(:freqs => :freqs1,
+                           :counts => :counts1,
+                           :rel_freqs => :rel_freqs1))
+
     combined[:freqs2] = raw_data[:bin2][:freqs]
     combined[:counts2] = raw_data[:bin2][:counts]
-    combined[:log2fc] = log2(combined[:freqs2]./combined[:freqs1])
+    combined[:rel_freqs2] = raw_data[:bin2][:rel_freqs]
+    combined[:log2fc] = log2(combined[:rel_freqs2]./combined[:rel_freqs1])
 
     nonnegs = combined[combined[:class] .!= :negcontrol, :]
     negcontrols = combined[combined[:class] .== :negcontrol, :log2fc]
