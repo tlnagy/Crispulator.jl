0dd4f0d3ab6cdf2cbd51ac42d0ff97850623ad91
Tamas Nagy <tamas@tamasnagy.com>
2016-06-14 13:38:51 -0700

    separate analysis tests for cleaner error messages

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,8 +14,8 @@ filenames = ["kdrelationships.jl",
              "qualitymetrics.jl",
              "diffcrisprtransfection.jl",
              "growth.jl",
-             "selectionmethods.jl",
-             "testanalyses.jl"]
+             "selectionmethods.jl"]
+
 for filename in filenames
     try
         include(filename)
@@ -30,7 +30,31 @@ for filename in filenames
             println()
         end
     end
+end
 
+analyses_path = normpath(joinpath(Base.source_dir(),"..",joinpath("src", "analyses")))
+analyses = readdir(analyses_path)
+analyses = analyses[find(x -> x != "common.jl", analyses)]
+# load common file first
+include(joinpath(analyses_path, "common.jl"))
+
+for analysis in analyses
+    try
+        include(joinpath(analyses_path, analysis))
+        tempfile = tempname()
+        main(tempfile, debug=true, quiet=true)
+        rm(tempfile)
+        println("\t\033[1m\033[32mPASSED\033[0m: $(analysis)")
+    catch e
+        errorfound = true
+        println("\t\033[1m\033[31mFAILED\033[0m: $(analysis)")
+        if fatalerrors
+            rethrow(e)
+        elseif !quiet
+            showerror(STDOUT, e, backtrace())
+            println()
+        end
+    end
 end
 
 if errorfound
