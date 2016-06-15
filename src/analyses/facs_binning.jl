19094e8474a4a151cc37553dd55593c0d07e6e64
Tamas Nagy <tamas@tamasnagy.com>
2016-06-15 13:26:33 -0700

    decrease range of scan for facs binning analysis

--- src/analyses/facs_binning.jl
+++ src/analyses/facs_binning.jl
@@ -5,8 +5,7 @@ function main(filepath; debug=false, quiet=false)
             :bottleneck_representation => [100, 1000, 1000],
             :seq_depth => [100, 1000, 1000],
             :σ => [1.0, 1.0, 0.5],
-            :bin_info => [Dict(:bin1 => (0.0, p), :bin2 => (1.0-p, 1.0)) for p in
-            [linspace(0.5, 0.1, 30); 0.1.^logspace(0.1, 0.3, 3)]]
+            :bin_info => [Dict(:bin1 => (0.0, p), :bin2 => (1.0-p, 1.0)) for p in [linspace(0.5, 0.025, 30)]]
         )
         num_runs = 100
     else
