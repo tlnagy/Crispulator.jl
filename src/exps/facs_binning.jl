684340c5d02e029a8b59e7b816ebb97c04f2e485
Tamas Nagy <tamas@tamasnagy.com>
2016-06-21 15:29:11 -0700

    fix brackets

--- src/exps/facs_binning.jl
+++ src/exps/facs_binning.jl
@@ -5,7 +5,7 @@ function main(filepath; debug=false, quiet=false)
             :bottleneck_representation => [100, 1000, 1000],
             :seq_depth => [100, 1000, 1000],
             :σ => [1.0, 1.0, 0.5],
-            :bin_info => [Dict(:bin1 => (0.0, p), :bin2 => (1.0-p, 1.0)) for p in [linspace(0.5, 0.025, 30)]]
+            :bin_info => [Dict(:bin1 => (0.0, p), :bin2 => (1.0-p, 1.0)) for p in linspace(0.5, 0.025, 30)]
         )
         num_runs = 100
     else
