daba05077da3c8d1104c3baaf2748d51819ec18d
Tamas Nagy <tamas@tamasnagy.com>
2016-06-21 18:00:50 -0700

    add subcommands to run.jl and prelim yaml parsing

--- src/run.jl
+++ src/run.jl
@@ -1,5 +1,7 @@
 (!isdir(Pkg.dir("ArgParse"))) && Pkg.add("ArgParse")
+(!isdir(Pkg.dir("YAML"))) && Pkg.add("YAML")
 using ArgParse
+using YAML
 
 function parse_cmdline()
     settings = ArgParseSettings(
@@ -10,16 +12,45 @@ function parse_cmdline()
     )
 
     @add_arg_table settings begin
+        "config"
+            action = :command
+            help = "run simulation using a YAML config file (easy)"
+        "exp"
+            action = :command
+            help = "run modified simulation from src/exps/ (hard)"
+        "ls"
+            action = :command
+            help = "list all modified simulations available"
+    end
+
+    settings["ls"].description = "Prints out the available modified simulation " *
+    "code. Simply prints out files in src/exps/."
+
+    @add_arg_table settings["exp"] begin
         "--debug"
             help = "Use smaller parameter space for diagnosing issues"
             action = :store_true
         "analysis_file"
-            help = "File from src/analyses/ to run"
+            help = "Run a modified simulation from the src/exps directory"
             required = true
         "output_file"
             help = "File to output results to [.CSV, .TSV, etc]"
             required = true
     end
+
+    settings["exp"].description = "Run a modified simulation. Much more" *
+    " flexible than using the configuration files, but also more complex"
+
+    @add_arg_table settings["config"] begin
+        "config_file"
+            help = "path to YAML configuration file to load"
+            required = true
+        "output_dir"
+            help = "path to output directory"
+            required = true
+    end
+
+    settings["config"].description
 
     settings.epilog = readall(normpath(joinpath(Base.source_dir(),"..","LICENSE")))
 
@@ -29,22 +60,35 @@ end
 
 function main()
     parsed_args = parse_cmdline()
-    # target_file = parsed_args[:analysis_file]
-    # filename = ARGS[2]
-    println("Using $(nprocs()) threads")
-    include(joinpath(Base.source_dir(), "simulation", "load.jl"))
-    print("Loading analysis code...")
-    target_file = parsed_args["analysis_file"]
-    target_full = joinpath(Base.source_dir(), "exps", target_file)
-    output_file = parsed_args["output_file"]
-    (!isfile(target_full)) && error("Please provide a valid analysis file to run")
-    (!isdir(dirname(abspath(output_file)))) && error("Please provide a valid directory for output")
-    include(joinpath(Base.source_dir(), "exps", "common.jl"))
-    include(target_full)
-    println("Done.")
-    output_file = compute_name(output_file)
-    println("Running $target_file and saving output in $output_file")
-    main(output_file, debug=parsed_args["debug"])
+
+    command = parsed_args["%COMMAND%"]
+
+    if command == "ls"
+        files = readdir(joinpath(Base.source_dir(), "exps"))
+        for file in files
+            (file == "common.jl") && continue
+            println(file)
+        end
+
+    elseif command == "exp"
+        println("Using $(nprocs()) threads")
+        include(joinpath(Base.source_dir(), "simulation", "load.jl"))
+        print("Loading analysis code...")
+        target_file = parsed_args[command]["analysis_file"]
+        target_full = joinpath(Base.source_dir(), "exps", target_file)
+        output_file = parsed_args[command]["output_file"]
+        (!isfile(target_full)) && error("Please provide a valid analysis file to run")
+        (!isdir(dirname(abspath(output_file)))) && error("Please provide a valid directory for output")
+        include(joinpath(Base.source_dir(), "exps", "common.jl"))
+        include(target_full)
+        println("Done.")
+        output_file = compute_name(output_file)
+        println("Running $target_file and saving output in $output_file")
+        main(output_file, debug=parsed_args[command]["debug"])
+    else
+        config = YAML.load_file(parsed_args[command]["config_file"])
+        println(config)
+    end
 end
 
 # fire up simulation if run using command line
