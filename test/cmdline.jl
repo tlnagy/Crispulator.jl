115c9ad776d2e3cac5fac54080e5247f673f0444
Tamas Nagy <tamas@tamasnagy.com>
2016-09-10 18:22:51 -0700

    fix tests broken in previous commit

--- test/cmdline.jl
+++ test/cmdline.jl
@@ -9,7 +9,7 @@ let s = build_arg_table()
 
     @test test_arg(["ls"]) == Dict{AbstractString, Any}("%COMMAND%" => "ls", "ls" => Dict{AbstractString, Any}())
     @test test_arg(["config", "CONFIG", "OUTPUT"]) ==
-        Dict{AbstractString,Any}("%COMMAND%"=>"config","config"=>Dict{AbstractString,Any}("config_file"=>"CONFIG","output_dir"=>"OUTPUT"))
+        Dict{AbstractString,Any}("%COMMAND%"=>"config","config"=>Dict{AbstractString,Any}("config_file"=>"CONFIG","output_dir"=>"OUTPUT", "no-graph" => false))
     @test test_arg(["exp", "-p", "1", "EXP", "OUTPUT"]) ==
         Dict{AbstractString,Any}("exp"=>Dict{AbstractString,Any}("output_file"=>"OUTPUT","analysis_file"=>"EXP","addprocs"=>1,"debug"=>false),"%COMMAND%"=>"exp")
     @test test_arg(["exp", "EXP", "OUTPUT"]) ==
