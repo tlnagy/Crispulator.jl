c57e540a38b286bed34f4cc3dc9cfd20c08cce17
autodocs <autodocs>
2016-11-22 23:41:43 +0000

    build based on 520e3ae

---
+++ release-0.1/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#CRISPulator-1",
+    "page": "Home",
+    "title": "CRISPulator",
+    "category": "section",
+    "text": "A pooled genetic screen simulatorPooled screens are very useful, but it is difficult to select all of the necessary parameters a priori. This work aims to explore the importance of various screen parameters and the relevance of design choices on the downstream analysis and interpretation."
+},
+
+{
+    "location": "index.html#Setup-1",
+    "page": "Home",
+    "title": "Setup",
+    "category": "section",
+    "text": "To run the simulation, you will need a recent version of Julia installed and in your PATH. Then navigate into the root directory of the project and run julia. Run the following command:julia -e 'Pkg.clone(pwd()); Pkg.build(\"Crispulator\")'this copies Crispulator over to the Julia package directory and installs all of its dependencies. "
+},
+
+{
+    "location": "index.html#Quickstart-1",
+    "page": "Home",
+    "title": "Quickstart",
+    "category": "section",
+    "text": "From the root directory of the project runjulia src/run.jl config example_config.yml ."
+},
+
+{
+    "location": "facs_growth.html#",
+    "page": "FACS vs Growth",
+    "title": "FACS vs Growth",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "facs_growth.html#FACS-vs-Growth-1",
+    "page": "FACS vs Growth",
+    "title": "FACS vs Growth",
+    "category": "section",
+    "text": ""
+},
+
+]}
