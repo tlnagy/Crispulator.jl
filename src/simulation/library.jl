f1d0b074637723e62e60b1a23a8234fc33056170
Tamas Nagy <tamas@tamasnagy.com>
2018-02-09 16:12:24 -0800

    update docs for multibin support

--- src/simulation/library.jl
+++ src/simulation/library.jl
@@ -106,7 +106,33 @@ type Library
 
     """
     Maximum phenotype categories mapped to their probability of being
-    selected and the distribution to draw from if they are selected
+    selected and the distribution to draw from if they are selected.
+
+    ## Example
+
+    The basic layout is a `Dict` mapping a class name to a tuple of the
+    probability of selecting this class and then the
+    [`Distributions.Sampleable`](https://juliastats.github.io/Distributions.jl/latest/types.html#Sampleable-1)
+    from which to draw a random phenotype from this class. The probabilities
+    across all the classes should add up to 1.
+
+    ```julia
+    max_phenotype_dists = Dict{Symbol, Tuple{Float64, Sampleable}}(
+        :inactive => (0.60, Delta(0.0)),
+        :negcontrol => (0.1, Delta(0.0)),
+        :increasing => (0.3, TruncatedNormal(0.1, 0.1, 0.025, 1)),
+    );
+    Library(max_phenotype_dists, CRISPRi());
+    ```
+
+    For example, here we are making three different classes of "genes": the
+    first group are :inactive, i.e. they have no phenotype, so we'll set their
+    phenotypes to 0.0 using a [`Simulation.Delta`](@ref). We'll also make them
+    60% of all the genes. The second group are the negative controls :negcontrol
+    (the only required group) which make up 10% of the population of genes and
+    also have no effect. The final group is :increasing which makes up 30% of
+    all genes and which are represented by a Normal(μ=0.1, σ=0.1) distribution
+    clamped between 0.025 and 1.
     """
     max_phenotype_dists::Dict{Int, Tuple{Symbol, Sampleable}}
     phenotype_probs::Categorical
--- src/simulation/processing.jl
+++ src/simulation/processing.jl
@@ -4,8 +4,9 @@ $(SIGNATURES)
 Given the raw data from [`Simulation.sequencing`](@ref) returns two DataFrames
 
 1. `guide_data`: This DataFrame contains the per-guide level data including the
-    log2 fold change in the normalized frequencies of each guide between the two
-    bins.
+    log2 fold change in the normalized frequencies of each guide between each
+    pairwise combination of bins. Thus, if there are `n` bins, then it computes
+    the log2 fold changes for the ``\\frac{n!}{2(n-2)!}`` combinations
 
 2. `gene_data`: This DataFrame contains the same information but grouped by
     gene. The log2 fold change data from the first DataFrame is used to calculate
@@ -14,54 +15,36 @@ Given the raw data from [`Simulation.sequencing`](@ref) returns two DataFrames
     measure of how consistently shifted the guides are of this gene versus the
     population of negative control guides. (see below for more info)
 
-A typical `guide_data` DataFrame contains the following columns:
+A typical 2 bin `guide_data` DataFrame contains the following columns:
 
-- `gene`: the gene ID of that this guide targets
-
-- `knockdown`: activity of the guide on 0 to 1 scale, where 1 is complete knockout
-
-- `barcodeid`: the ID of this specific guide
-
-- `theo_phenotype`: expected phenotype of this guide, generally a -1 to 1 scale
-
-- `behavior`: whether the target gene displays a linear or sigmoidal response to knockdown
-
-- `class`: whether the target gene has a positive, negative, or no phenotype during screening
-
-- `initial_freq`: frequency of guide post-transfection (see [`Simulation.transfect`](@ref))
-
-- `counts_bin1`: the raw number of reads for each guide in the first bin
-
-- `freqs_bin1`: the number of reads for each guide divided by the total number of reads in this bin
-
-- `rel_freqs_bin1`: the frequency of each guide divided by the median frequency of negative control guides
-
-- `counts_bin2`: the raw number of reads for each guide in the second bin
-
-- `freqs_bin2`: the number of reads for each guide divided by the total number of reads in this bin
-
-- `rel_freqs_bin2`: the frequency of each guide divided by the median frequency of negative control guides for this bin
-
-- `log2fc_bin2`: the log2 fold change in relative guide frequencies between the two bins
+| Column Name  | Meaning  |
+|:--|:--|
+| `gene` | the gene ID of that this guide targets|
+| `knockdown` | activity of the guide on 0 to 1 scale, where 1 is complete knockout|
+| `barcodeid` | the ID of this specific guide|
+| `theo_phenotype` | expected phenotype of this guide, generally a -1 to 1 scale|
+| `behavior` | whether the target gene displays a linear or sigmoidal response to incomplete knockdown (see [`Simulation.Library`](@ref) for more details)|
+| `class` | which phenotype distribution the target gene was drawn from (see [`Simulation.Library`](@ref) for more details). Serves as the "ground truth" label against which screen performance is evaluated, e.g. with [`Simulation.auprc`](@ref) |
+| `initial_freq` | frequency of guide post-transfection (see [`Simulation.transfect`](@ref))|
+| `counts_bin1` | the raw number of reads for each guide in the first bin|
+| `freqs_bin1` | the number of reads for each guide divided by the total number of reads in this bin|
+| `rel_freqs_bin1` | the frequency of each guide divided by the median frequency of negative control guides|
+| `counts_bin2` | the raw number of reads for each guide in the second bin|
+| `freqs_bin2` | the number of reads for each guide divided by the total number of reads in this bin|
+| `rel_freqs_bin2` | the frequency of each guide divided by the median frequency of negative control guides for this bin|
+| `log2fc_bin2_div_bin1` | the log2 fold change in relative guide frequencies between `bin2` and `bin1`, equivalent to `log2(rel_freqs_bin2/rel_freqs_bin1)` |
 
 A typical `gene_data` DataFrame contains the following data:
 
-- `gene`: this gene's ID
-
-- `behavior`: whether this gene displays a linear or sigmoidal response to knockdown
-
-- `class`: whether this gene has a positive, negative, or no phenotype during screening
-
-- `mean`: the mean log 2 fold change in relative frequencies between the two bins
-    for all the guides targeting this gene.
-
-- `pvalue`: the -log10 pvalue of the log2 fold changes of all guides targeting
-    this gene as computed by the non-parametric Mann-Whitney U-test. A measure
-    of the consistency of the log 2 fold changes[^1]
-
-- `absmean`: absolute value of `mean` per-gene
-
-- `pvalmeanprod`: `mean` multiplied with the `pvalue` per-gene
+| Column Name  | Meaning  |
+|:--|:--|
+| `gene` | this gene's ID|
+| `class` | see above |
+| `behavior` | see above |
+| `mean_bin2_div_bin1` | the mean log 2 fold change in relative frequencies between from `bin1` to `bin2` for all the guides targeting this gene. Calculated as ``\\frac{1}{k}\\sum_k \\text{log2fc_bin2_div_bin1}_k`` for the ``k`` guides targeting each gene |
+| `pvalue_bin2_div_bin1` | the -log10 pvalue of the log2 fold changes of all guides targeting this gene as computed by the non-parametric Mann-Whitney U-test. A measure of the consistency of the log 2 fold changes[^1]|
+| `absmean_bin2_div_bin1` | absolute value of `mean_bin2_div_bin1` per-gene |
+| `pvalmeanprod_bin2_div_bin1` | `mean_bin2_div_bin1` multiplied with the `pvalue_bin2_div_bin1` per-gene|
 
 # Further reading
 
